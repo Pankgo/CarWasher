b7d1535a3ea302cb4dbf4c803970d1725e477b28
myname0801 <147930457+myname0801@users.noreply.github.com>
2024-01-25 13:36:26 +0900

    /signup 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import UserInfo from "./Pages/UserInfo";
+import Reserve from "./Pages/Reserve";
+import UserInfo from "./Functions/UserInfo";
+import Login from "./Functions/Login";
+import Signup from "./Functions/Signup";
 
 function App() {
+  const [islogin, setislogin] = useState(false)
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
-      <Route path="/Account" element={<Account/>}></Route>
+      <Route path="/Account" element={islogin ? <UserInfo setislogin={setislogin}/> : <Login setislogin={setislogin}/>}></Route>
+      <Route path="/Reserve" element={islogin ? <Reserve setislogin={setislogin}/> : <Login setislogin={setislogin}/>}></Route>
+      <Route path="/Signup" element={<Signup/>}></Route>
     </Routes>
     </BrowserRouter>
   );
